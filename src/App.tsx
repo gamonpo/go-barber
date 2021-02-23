71c51b9919663ef04a4430d0e6a568f95e9fe35e
gamonpo <gamonpo@outlook.com>
2021-02-23 12:03:19 -0300

    Ref Splash Screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 
 import { StatusBar } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 import AppProvider from './hooks';
 
 import Routes from './routes';
 import colors from './style/colors';
 
-const App: React.FC = () => (
-  <NavigationContainer>
-    {/* <AppProvider> */}
-    <StatusBar
-      barStyle="light-content"
-      backgroundColor={colors.color_five}
-      translucent
-    />
-    <Routes />
-    {/* </AppProvider> */}
-  </NavigationContainer>
-);
+const App: React.FC = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      {/* <AppProvider> */}
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={colors.color_five}
+        translucent
+      />
+      <Routes />
+      {/* </AppProvider> */}
+    </NavigationContainer>
+  );
+};
 
 export default App;
